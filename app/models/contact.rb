f1dc6a7caf9fb5ead1ce95a76f43d3d71ea96f5b
andreia tarifa <andreia.p.tarifa@gmail.com>
2022-04-26 10:52:05 -0300

    feat: add validations to cpfcnpj

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,6 @@
 class Contact < ApplicationRecord
     validates :name,  length: {minimum:4}
-    validates :active, :birthday, :email, :mobile, :advertising, presence: true
+    validates :active, :birthday, :email, :mobile, :advertising, :cpfcnpj, presence: true
+    validates :cpfcnpj, length: { in: 11..14 }
+    validates :cpfcnpj, format: { with: /\A\d+\z/, message: "only allows numbers" }
 end
