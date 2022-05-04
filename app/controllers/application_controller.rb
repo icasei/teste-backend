bd07a0e1140407132c0f93f7a371b8e0fc03fd3a
Michel Oliveira <michelsantosoliveira@hotmail.com>
2022-05-03 22:36:55 -0300

    modificação no app_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  # Adcionado a linha abaixo devido falha de verificação no token após requisição POST
+  protect_from_forgery unless: -> { request.format.json? }
 end
